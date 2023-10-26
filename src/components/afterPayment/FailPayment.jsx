8c485f63da81744dc692ec5a22696a18099e7da0
Bhumi <bhumione9545@gmail.com>
2023-10-26 21:02:57 +0530

    Added payment fail check

--- src/components/afterPayment/FailPayment.jsx
+++ src/components/afterPayment/FailPayment.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const FailPayment = () => {
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      window.location.href = '/'
+    }, 3000)
+
+  }, [])
+
+
   return (
     <div className='flex items-center justify-center w-full h-full'>
-      <h1 className='text-2xl font-bold'>Payment Failed</h1>
+      <h1 className='text-2xl font-bold'>Payment Failed, redirecting you the cart, please try again...</h1>
     </div>
   )
 }
